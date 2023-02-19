543a0c6639c38d9967a2c93e4f6d8a0bf09531f2
Самарин Даниил <daniil_samarin@mail.ru>
2023-02-19 17:14:16 +0700

    Добавьте файлы проекта.

---
+++ RomanNumbersCalculator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+using System;
+using ReactiveUI;
+using RomanNumbersCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RomanNumbersCalculator.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string currentOperationStringRepresentation = "";
+        private string currentNumberStringRepresentation = "";
+        private string Stack<RomanNumberExtend> StackRomanNumbers = new Stack<RomanNumberExtend>;
+
+
+    }
+}
---
+++ UITestsForRomanNumbersCalculator/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace UITestsForRomanNumbersCalculator
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
